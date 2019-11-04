76e7937c3b46238991f5ccbe8a232ab0d71d9c56
unknown <lana.manher@gmail.com>
2019-11-04 19:01:24 +0100

    commit

--- pp.py
+++ pp.py
@@ -1,11 +1,16 @@
-import re
-import requests
+import csv
 import json
+import os
+import requests
+import sys
+import re
 
 with open('kripto.html', encoding='utf-8') as f:
     vsebina = f.read()
 
 vzorec = (
+    r'<a.*class=".*name.*".*>(?P<name>.*)</a>\s*'
+    r'</td>\s*'
     r'<td class=".*col-symbol">(?P<simbol>[A-Z]{0,})</td>\s*'
     r'<td class="no-wrap market-cap.*>\s*\$*(?P<market_cap>.*)\s*</td>\s*'
     r'<td.*>\s*'
@@ -25,6 +30,37 @@ kriptovalute = []
 for zadetek in re.finditer(vzorec, vsebina):
     kriptovalute.append(zadetek.groupdict())
     count += 1
+print(count)
+
+for slovar in kriptovalute:
+    if slovar['market_cap'] == '?':
+        slovar['market_cap'] = None
+    else:
+        slovar['market_cap'] = int(slovar['market_cap'].replace(',', ''))
+
+for slovar in kriptovalute:
+    if slovar['volume'] == '?' or slovar['volume'] == 'Low Vol':
+        slovar['volume'] = None
+    else:
+        slovar['volume'] = int(slovar['volume'].replace(',', ''))
+
+for slovar in kriptovalute:
+    if slovar['circulating_supply'] == 'None':
+        slovar['circulating_supply'] = None
+    else:
+        slovar['circulating_supply'] = float(slovar['circulating_supply'])
+
+for slovar in kriptovalute:
+    slovar['price'] = float(slovar['price'])
+    slovar['one_hour'] = float(slovar['one_hour'])
+    slovar['one_day'] = float(slovar['one_day'])
+    slovar['one_week'] = float(slovar['one_week'])
 
 with open('kriptovalute.json', 'w') as f:
     json.dump(kriptovalute, f, indent=2)
+
+with open('kriptovalute.csv', 'w', encoding='utf-8') as csv_datoteka:
+    writer = csv.DictWriter(csv_datoteka, fieldnames=['name', 'simbol', 'market_cap', 'price', 'circulating_supply', 'volume', 'one_hour', 'one_day', 'one_week'])
+    writer.writeheader()
+    for slovar in kriptovalute:
+        writer.writerow(slovar)
